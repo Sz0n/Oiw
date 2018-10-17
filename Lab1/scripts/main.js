be9254e47d3cd82cedee47f0c78466e346fe8700
Kamil Paul <kamilrsw@gmail.com>
2018-10-17 10:58:56 +0200

    Aktualizacja JavaScript

--- Lab1/scripts/main.js
+++ Lab1/scripts/main.js
@@ -1,2 +1,16 @@
+var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello World!';
+function setUserName() {
+  var myName = prompt('Wprowadz swoje imię.');
+  localStorage.setItem('Imię', myName);
+  myHeading.textContent = 'Miłego dnia ' + myName + ';)';
+}
+if(!localStorage.getItem('Imię')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('Imię');
+  myHeading.textContent = 'Miłego dnia nieznajomy ;)';
+}
+myButton.onclick = function() {
+  setUserName();
+}
