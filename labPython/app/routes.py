06275557f08420434feff9f538587a0ffef9eb32
Kamil Paul <kamilrsw@gmail.com>
2018-12-01 20:50:46 +0100

    Login & Registration

--- labPython/app/routes.py
+++ labPython/app/routes.py
@@ -5,6 +5,11 @@ from app import db
 from werkzeug.urls import url_parse
 from app.forms import PostForm
 from flask import request
+from flask_login import current_user, login_user
+from app.models import User
+from flask_login import logout_user
+from app.forms import LoginForm
+from app.forms import RegistrationForm
 
 @app.route('/')
 @app.route('/index')
@@ -27,3 +32,36 @@ def add_post():
 
 
 	return render_template('add_post.html', title='Add Post', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))    
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Registered')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
