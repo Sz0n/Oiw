5e3570458be0945d80cac37dc6c5dd7513fc10c6
Kamil Paul <kamilrsw@gmail.com>
2018-12-03 09:07:51 +0100

    LAb 7 - login/session&cookies

--- labPython/app/routes.py
+++ labPython/app/routes.py
@@ -1,15 +1,9 @@
-from app import app
-from flask import render_template, url_for, redirect, flash
-from app.models import Post
-from app import db
+from app import app, db
 from werkzeug.urls import url_parse
-from app.forms import PostForm
-from flask import request
-from flask_login import current_user, login_user
-from app.models import User
-from flask_login import logout_user
-from app.forms import LoginForm
-from app.forms import RegistrationForm
+from flask import render_template, url_for, redirect, flash, request, make_response
+from flask_login import current_user, login_user, logout_user
+from app.models import Post, User
+from app.forms import PostForm, LoginForm, RegistrationForm
 
 @app.route('/')
 @app.route('/index')
@@ -44,13 +38,24 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
+
+        userCookie = request.form['username']
+        resp = make_response(redirect('index'))
+        resp.set_cookie('userCookie', userCookie)
+        next_page = request.args.get('next')
+
+        if not next_page or url_parse(next_page).netloc !='':
+        	next_page = url_for('index')
+        	return resp
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index'))    
+    resp = make_response(redirect('index'))
+    resp.delete_cookie('user')
+    return resp
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
