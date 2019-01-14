dd47d4a5810485a076dc886359c59b706d4e22f0
Kamil Paul <kamilrsw@gmail.com>
2019-01-14 22:32:25 +0100

    naprawiam

--- labPython/app/__init__.py
+++ labPython/app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
-from app import routes, models
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,27 +14,4 @@ Bootstrap(app)
 login = LoginManager(app)
 login.login_view = 'login'
 
-def create_app(config_class=Config):
-    # ...
-    if not app.debug and not app.testing:
-        # ...
-
-        if app.config['LOG_TO_STDOUT']:
-            stream_handler = logging.StreamHandler()
-            stream_handler.setLevel(logging.INFO)
-            app.logger.addHandler(stream_handler)
-        else:
-            if not os.path.exists('logs'):
-                os.mkdir('logs')
-            file_handler = RotatingFileHandler('logs/microblog.log',
-                                               maxBytes=10240, backupCount=10)
-            file_handler.setFormatter(logging.Formatter(
-                '%(asctime)s %(levelname)s: %(message)s '
-                '[in %(pathname)s:%(lineno)d]'))
-            file_handler.setLevel(logging.INFO)
-            app.logger.addHandler(file_handler)
-
-        app.logger.setLevel(logging.INFO)
-        app.logger.info('Microblog startup')
-
-    return app
+from app import routes, models
